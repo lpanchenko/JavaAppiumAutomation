f82190a1a0373834325ed0c7b92795c0e2affda6
lpanchenko <liuba.panchenko@gmail.com>
2021-01-12 23:01:29 -0500

    Add test GetClass Number test that passes if getClassName method returns value is bigger that 45

--- MainClass.java
+++ MainClass.java
@@ -1,7 +1,11 @@
 public class MainClass
 {
+    private static int class_number = 20;
+
     public static int getLocalNumber()
     {
         return 14;
     }
+
+    public static int getClassNumber() {return class_number;}
 }
--- MainClassTest.java
+++ MainClassTest.java
@@ -7,4 +7,11 @@ public class MainClassTest {
         Assert.assertEquals("Returned value have to be 14",
                 14, MainClass.getLocalNumber());
     }
+
+    @Test
+    public void testGetClassNumber(){
+        Assert.assertTrue("Returned value should be bigger that 45, actual is " + MainClass.getClassNumber(),
+                MainClass.getClassNumber() > 45);
+
+    }
 }
