68e24ee5419053d3161a70c4b42701320e94dd00
lpanchenko <liuba.panchenko@gmail.com>
2021-01-24 20:58:50 -0500

    Change orientation to portrait before each test

--- FirstTest.java
+++ FirstTest.java
@@ -35,6 +35,9 @@ public class FirstTest {
         capabilities.setCapability("app", "C:/Users/elbit/Documents/Repositories/JavaAppiumAutomation/apks/org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+        if (driver.getOrientation() != ScreenOrientation.PORTRAIT)
+            driver.rotate(ScreenOrientation.PORTRAIT);
     }
 
     @After
